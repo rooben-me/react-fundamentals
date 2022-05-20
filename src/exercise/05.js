7116efc65ae3bcae78cd021bc01f90ea7018b1aa
rooben-me <rubankumar.nkl@gmail.com>
2022-05-20 18:04:17 +0530

    finished step-5 all noraml excercise

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,20 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const Box = ({className = '', ...otherProps}) => {
+  return <div className={`box ${className}`} {...otherProps} />
+}
+
+const smallBox = (
+  <Box
+    className="box--small"
+    style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+  >
+    small lightblue box
+  </Box>
+)
+const mediumBox = <Box style={{backgroundColor: 'pink'}}>small pink box</Box>
+const largeBox = <Box style={{backgroundColor: 'orange'}}>small orange box</Box>
 
 function App() {
   return (
